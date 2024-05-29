adf982bf5fab45de511abf4bb3959632df0ddc17
Rek Dev <rekdev@keemail.me>
2024-05-29 17:59:37 -0600

    Add experimental rust merge sort algorithm

---
+++ Merge_Sort/merge_sort.rs
@@ -0,0 +1,54 @@
+fn merge(arr: &mut [isize], left: &mut [isize], right: &mut [isize]) {
+    let left_len = left.len();
+    let right_len = left.len();
+
+    let mut i = 0;
+    let mut j = 0;
+    let mut k = 0;
+
+    while i < left_len && j < right_len {
+        if left[i] <= right[j] {
+            arr[k] = left[i];
+            i += 1;
+        } else {
+            arr[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while i < left_len {
+        arr[k] = left[i];
+        i += 1;
+        k += 1;
+    }
+
+    while j < right_len {
+        arr[k] = right[j];
+        j += 1;
+        k += 1;
+    }
+}
+
+fn merge_sort(arr: &mut [isize]) {
+    let n = arr.len();
+
+    if n > 1 {
+        let mid = n / 2;
+
+        let (left, right) = arr.split_at_mut(mid);
+
+        merge_sort( left);
+        merge_sort( right);
+
+        merge(arr, left, right);
+    }
+}
+
+fn main() {
+    let mut arr = [8, 10, 7, 1, 4, 6, 2, 5, 9, 3];
+
+    merge_sort(&mut arr);
+
+    println!("{:?}", arr);
+}
